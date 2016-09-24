2c14aaca154415162256b90049cb9e5cc47878b6
joefraley <jose.fraley@gmail.com>
2016-09-23 18:27:53 -0700

    Webmentions up and running.

--- src/components/blog/blog-post.js
+++ src/components/blog/blog-post.js
@@ -4,32 +4,44 @@ import { createMarkup, getPost } from '../../utilities.js'
 
 import CSS from './styles.scss'
 
-
 export const BlogTags = props => {
   return <p className={ `${ CSS.tags } p-category` }>tagged: { props.tags }</p>
 }
 
-export const getWebMentions = postName => {
-  console.log('getting mentions', postName)
-  const target = `https://ratticusscript.firebaseapp.com/posts/${ postName }`
-  const mentions = request('GET', `http://webmention.io/api/mentions?target=${ target }`).getBody()
-  console.log(mentions)
-  console.log(typeof mentions)
-  return mentions
+export const getWebMention = postName => {
+  const mentions = require(`../../../utilities/webmentions/posts/${ postName }.json`)
+  return mentions.links
+}
+
+export const getAllWebMentions = () => {
+  const mentions = require.context(`../../../utilities/webmentions/posts`)
+  return mentions.keys().map(mention => mentions(mention).links)
 }
 
 export const WebMention = props => {
-  console.log('webmention:', props)
-  return <div key={ props.key }>
-    <div className="comment-author">
-      { props.mention.author && props.mention.author.name ? <p>{ props.mention.author }</p> : null }
-      { props.mention.author && props.mention.author.url ? <a href={ props.mention.author.url }>{ props.mention.author }</a> : null }
-      { props.mention.author && props.mention.author.photo ? <img src={ props.mention.author.photo } /> : null }
+  const { data } = props.mention
+  const { author, content, url, published } = data
+  return <div className={ CSS.mention }>
+    <div className={ CSS.commentAuthor }>
+      { data.author && author.photo ? <img className={ CSS.authorImage } src={ author.photo } /> : null }
+      { data.author && author.name ? <a href={ author.url } className={ CSS.authorName }>{ author.name }</a> : null }
     </div>
-    <p>{ props.mention.content }</p>
-    <a href={ props.mention.url }>{ props.mention.url }</a>
-    <time>{ props.mention.published }</time>
+    <p>{ content }</p>
+    <a href={ url }>Original post here</a>
+    <h3 className={ CSS.date }>
+      <time className="dt-published" dateTime={ published }>{ Moment(published).calendar() }</time>
+    </h3>
   </div>
+}
+
+export const ListOfMentions = props => {
+  return <ul className={ CSS.mentions }>
+    { getWebMention(props.postName)
+        .map((mention, index) => {
+          return <WebMention mention={ mention } key={ index }/>
+        })
+    }
+  </ul>
 }
 
 export const BlogPost = props => {
@@ -43,7 +55,9 @@ export const BlogPost = props => {
       <a className="u-url" href={`https://ratticusscript.firebaseapp.com/posts/${props.params.id}`} style={{display: 'none'}}></a>
       <a rel="author" className="p-author h-card" href="https://ratticusscript.firebaseapp.com/" style={{display:'none'}}>{ meta.author }</a>
       <h1 className="p-name">{ meta.title }</h1>
-      <h3 className={ CSS.date }><time className="dt-published" dateTime={ meta.date }>{ date }</time></h3>
+      <h3 className={ CSS.date }>
+        <time className="dt-published" dateTime={ meta.date }>{ date }</time>
+      </h3>
     </div>
 
     {/* Actual blog content */}
@@ -51,5 +65,7 @@ export const BlogPost = props => {
 
     <p>Cross posted on Medium at <a href="https://medium.com/@joefraley">https://medium.com/@joefraley</a></p>
     <BlogTags tags={ meta.tags } />
+    <h2>Mentions on the Web</h2>
+    <ListOfMentions postName={ props.params.id }/>
   </article>
 }
