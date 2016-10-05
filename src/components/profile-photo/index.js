3e91cda7d9b5fb8b5e5a1a578ec648b83d8d0268
joefraley <jose.fraley@gmail.com>
2016-10-05 13:46:01 -0700

    Includes alt-text on profile photo.

--- src/components/profile-photo/index.js
+++ src/components/profile-photo/index.js
@@ -3,5 +3,5 @@ import PICTURE from './close-up.png'
 import CSS from './styles.scss'
 
 export const ProfilePhoto = props => {
-  return <img className={ `${ CSS.picture } u-photo` } src={ PICTURE } />
+  return <img className={ `${ CSS.picture } u-photo` } src={ PICTURE } alt="Joe Fraley, smiling and pretty."/>
 }
