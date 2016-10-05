22ff9d88b380f9d3b0500ad273c4b208d89e9d58
joefraley <jose.fraley@gmail.com>
2016-10-05 11:22:39 -0700

    Add links to twitter hashtags.

--- src/components/note/index.js
+++ src/components/note/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Moment from 'moment'
 import CSS from './styles.scss'
+import { createMarkup } from '../../utilities.js'
+
+export const buildHashtagLink = (match, offset, string) => {
+  const hashtag = match.substring(1)
+  return `<a href="https://twitter.com/hashtag/${ hashtag }" target="_">${ match }</a>`
+}
+
+export const filterForHashtags = string => string.replace(/#\S+/g, buildHashtagLink)
 
 export const Note = ({ note }) => {
   const date = Moment(note.meta.attributes.date).format('LLLL')
@@ -9,10 +17,10 @@ export const Note = ({ note }) => {
     <header className={ CSS.previewHead }>
       <h4 className={ CSS.date }>{ date }</h4>
       { note.media
-        ? <img className={ CSS.noteImage } src={ note.media[0].media_url_https}></img>
+        ? <img className={ CSS.noteImage } src={ note.media[0].media_url_https} alt={ note.content }></img>
         : null
       }
-      <p>{ note.content }</p>
+      <p dangerouslySetInnerHTML={ createMarkup(filterForHashtags(note.content)) }></p>
     </header>
 
     {
